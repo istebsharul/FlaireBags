689bbac842f708ac7aa44b55e64be98448081153
istebsharul <istebsharulbari919@gmail.com>
2023-08-03 19:14:52 +0530

    Initial Comit

---
+++ Template/_banner-ads.php
@@ -0,0 +1,8 @@
+<!-- Banner Ads  -->
+<section id="banner_adds">
+    <div class="container py-5 text-center">
+        <img src="./assets/ad1.png" alt="banner1" class="img-fluid">
+        <img src="./assets/ad2.png" alt="banner2" class="img-fluid">
+    </div>
+</section>
+<!-- !Banner Ads  -->
